a3090adc305f9e57a8818818ad8a1df0955cc571
康铠 <50852262+kk19990709@users.noreply.github.com>
2020-09-16 22:26:20 +0800

    完成MultiCPU

--- new/EXE.v
+++ new/EXE.v
@@ -6,23 +6,25 @@ module EXE(
 		input wire i_EXE_dmemWe, i_EXE_regWe, i_EXE_sA1, i_EXE_sA2, i_EXE_sB, i_EXE_sWD,
 
 		input wire [5:0] i_EXE_ALUop,
-		input wire [31:0] i_EXE_rd1, i_EXE_rd2, i_EXE_num, i_EXE_shrmt,
+		input wire [31:0] i_EXE_rd1, i_EXE_rd2, i_EXE_num, i_EXE_sa,
 
 		output wire o_EXE_dmemWe, o_EXE_regWe, o_EXE_sWD,
 
 		output wire [4:0] o_EXE_WRA,
-		output wire [31:0] o_EXE_ALUout
+		output wire [31:0] o_EXE_aluOut
+		output wire [31:0] o_EXE_rd2
 	);
 	
 	reg EXE_dmemWe, EXE_regWe, EXE_sA1, EXE_sA2, EXE_sB, EXE_sWD;
 	reg [4:0] EXE_WRA;
 	reg [5:0] EXE_ALUop;
-	reg [31:0] EXE_rd1, EXE_rd2, EXE_num, EXE_shrmt;
+	reg [31:0] EXE_rd1, EXE_rd2, EXE_num, EXE_sa;
 
 	assign o_EXE_dmemWe = EXE_dmemWe;
 	assign o_EXE_regWe = EXE_regWe;
 	assign o_EXE_sWD = EXE_sWD;
 	assign o_EXE_WRA = EXE_WRA;
+	assign o_EXE_rd2 = EXE_rd2;
 
 	always @(posedge clk or negedge rstn) begin
 		if (!rstn) begin
@@ -37,7 +39,7 @@ module EXE(
 			EXE_rd1 <= 0;
 			EXE_rd2 <= 0;
 			EXE_num <= 0;
-			EXE_shrmt <= 0;
+			EXE_sa <= 0;
 		end
 		else begin
 			EXE_dmemWe <= i_EXE_dmemWe;
@@ -51,14 +53,14 @@ module EXE(
 			EXE_rd1 <= i_EXE_rd1;
 			EXE_rd2 <= i_EXE_rd2;
 			EXE_num <= i_EXE_num;
-			EXE_shrmt <= i_EXE_shrmt;
+			EXE_sa <= i_EXE_sa;
 		end
 	end
 
 	wire [31:0] EXE_tempA, EXE_A, EXE_B;
 
 	mux #32 muxA1(
-		.in0			(EXE_shrmt),
+		.in0			(EXE_sa),
 		.in1			(EXE_num),
 		.select			(EXE_sA1),
 		.out 			(EXE_tempA));
@@ -76,5 +78,5 @@ module EXE(
 		.i_ALU_srcA		(EXE_A),
 		.i_ALU_srcB		(EXE_B),
 		.i_ALU_op		(EXE_ALUop),
-		.o_ALU_aluOut	(o_EXE_ALUout));
+		.o_ALU_aluOut	(o_EXE_aluOut));
 endmodule
